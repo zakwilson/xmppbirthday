1fc318d5320a72dba4ca8815eddcba21cc90287c
Zak Wilson <zak.wilson@gmail.com>
2014-01-05 20:32:33 -0500

    Oh, right, the rest of it

--- src/xmppbirthday/core.clj
+++ src/xmppbirthday/core.clj
@@ -1,5 +1,5 @@
 (ns xmppbirthday.core
-  (:require [quit-yo-jabber :as xmpp]
+  (:require [quit-yo-jibber :refer :all :as xmpp]
             [clj-time.core :as time]))
 
 
@@ -11,8 +11,32 @@
 (def config (atom {}))
 
 (defn load-config []
-  (swap! config (read-config)))
+  (swap! config merge (read-config)))
 
-(defn connect-bot [params]
+(declare conn)
+
+(defn connect-bot [& [params]]
   (let [params (merge @config params)]
-    (xmpp/start-bot params)))
+    (def conn
+      (xmpp/make-connection params))))
+
+(defn disconnect-bot [& [bot]]
+  (xmpp/close-connection (or bot conn)))
+
+(defn make-birthday [birthday]
+  (time/date-time (time/year (time/now))
+                  (:month birthday)
+                  (:day birthday)))
+
+(defn is-birthday? [person]
+  (let [birthday (make-birthday (:birthday person))]
+    (time/within? (time/interval (time/now)
+                                 (time/plus (time/now)
+                                            (time/days 1)))
+                  (time/now))))
+
+(defn wish-if-birthday [person]
+  (when (is-birthday? person)
+    (send-message conn
+                  (:xmpp-id person)
+                  "Happy birthday! (see https://github.com/zakwilson/xmppbirthday)")))
